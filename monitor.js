c50f88a60cf65340b4535c66bf7607b5d212202d
undetect <andyguo12@gmail.com>
2020-07-28 11:04:03 -0400

    Ok

---
+++ monitor.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+const colors = require('colors')
+require('dotenv').config()
+
+
+let monitoredSites = [
+
+'https://www.finishline.com/public/5f1a9c3afno20161dd820a65a151e9a',
+'https://www.shoepalace.com/cdn-cgi/bm/cv/1284585713/api.js'
+
+]
+
+
+function initMonitor(){
+    
+}
+
+
+initMonitor()
