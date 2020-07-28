8cde0ddd39ff405966db4adcc8aa789e01563c02
undetect <andyguo12@gmail.com>
2020-07-28 18:30:48 -0400

    feat: added in charLimit for webhook and basic monitor is here!

--- src/monitor.js
+++ src/monitor.js
@@ -7,11 +7,16 @@ const {
     Webhook,
     MessageBuilder
 } = require('discord-webhook-node');
+const {
+    ifError
+} = require('assert');
+const {
+    resolve
+} = require('path');
 require('dotenv').config()
 
 
 let monitoredSites = { // ORGANIZED BY PREFERRED FILENAME : FILE URL
-
     "finishline": 'https://www.finishline.com/public/5f1a9c3afno20161dd820a65a151e9a', // MONITORS FINISHLINE AKAM FILE 
     "shoepalace": 'https://www.shoepalace.com/cdn-cgi/bm/cv/1284585713/api.js' // MONITORS SHOE PALACE CLOUFFLAR FINGERPRINTING FILE
 
@@ -43,13 +48,13 @@ function initMonitor() { // SAVES ALL SITE CONTENT To INDIVIDUAL TEXT FILES
             fetch(monitoredSites[fileName]) // GET THE FILE CONTENT
                 .then(resp => resp.text())
                 .then(content => {
-                    fs.writeFile(`../content/${fileName}.txt`, content, function (err) { // CREATION AND WRITING OF CONTENT FILE
+                    fs.writeFile(`../content/${fileName}.txt`, content.replace("document['onreadystatechange']", "thisisaddedin(yourmom)") + "this is added in stupid", function(err) {
                         if (err) monitorLog(fileName, err, 'err');
                         monitorLog(fileName, 'Successfully pulled initial content', 'log')
-                        startMonitor()
                     })
                 })
         })
+        startMonitor()
         webhook.send(initEmbed) // SENDS WEBHOOK CONTAINING INITIALIZATION INFORMATION (LIST OF ALL FILENAME/URL REQUESTED)
             .catch(error => {
                 monitorLog('Initializing', error, "err")
@@ -61,10 +66,50 @@ function initMonitor() { // SAVES ALL SITE CONTENT To INDIVIDUAL TEXT FILES
 
 
 function startMonitor() { // MONITORS CONTENT EVERY X DELAY AND ALERTS IF THERE IS A CHANGE OR NOT
-    setTimeout(() => {
+    setInterval(() => {
         Object.keys(monitoredSites).forEach(fileName => {
             fetch(monitoredSites[fileName])
                 .then(resp => resp.text())
+                .then(content => {
+                    let oldData = fs.readFileSync(`../content/${fileName}.txt`, {
+                        encoding: 'utf8'
+                    })
+                    if (oldData.toString() == content.toString()) {
+                        monitorLog(fileName, 'File content has remained the same', 'same')
+                    } else {
+                        let findDiff = new Promise((resolve) => {
+                            let changeArr = [" ", " "]
+                            oldData.split('').forEach(function(val, i) {
+                                if (val != content.charAt(i)) changeArr[0] += val; // FINDS WHAT CONTENT WAS ADDED
+                            })
+                            content.split('').forEach(function(val, i) {
+                                if (val != oldData.charAt(i)) changeArr[1] += val; // FINDS WHAT CONTENT WAS ADDED
+                            })
+                            resolve(changeArr)
+                        })
+                        findDiff.then((changeArr) => {
+                            let added = changeArr[0]
+                            let removed = changeArr[1]
+                            const changeEmbed = new MessageBuilder() // WEBHOOK BUILDER
+                                .setTitle('FileTrackr Detected A Change!')
+                                .setDescription('`A file content change has been detected.`')
+                                .setColor('#000000')
+                                .setImage('https://i.imgur.com/Zn5wxLF.png')
+                                .addField(`${fileName}`, `${monitoredSites[fileName]}`)
+                                .addField(`Added`, "`" + `${added.substring(0,process.env.charLimit)}` + "`")
+                                .addField(`Removed`, "`" + `${removed.substring(0,process.env.charLimit)}` + "`")
+                                .setFooter('Created by @retryafter', 'https://i.imgur.com/Zxh45Hf.png')
+                                .setTimestamp()
+                            let webhook = new Webhook(process.env.webhookURL);
+                            webhook.send(changeEmbed)
+                                .catch(error => {
+                                    monitorLog('Initializing', error, "err")
+                                })
+                            monitorLog(fileName, 'File content changed', 'change')
+                        })
+                    }
+
+                })
         })
     }, process.env.monitorDelay)
 }
